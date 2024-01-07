3319262759b912469729076de327fd6c3c020c9e
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-07 15:22:16 -0600

    implement basic signals

--- src/cardreader.h
+++ src/cardreader.h
@@ -3,13 +3,51 @@
 
 #include "stationcomponent.h"
 
+/* A CardReader object is used for interacting with and receiving data from the station's built-in card reader. */
 class CardReader : public StationComponent
 {
-    Q_OBJECT
+    //Q_OBJECT
 public:
-    explicit CardReader(QObject *parent = nullptr);
+    explicit CardReader(StationComponent *parent = nullptr);
 
 signals:
+    // Signal for when a card is swiped
+    void swiped();
+    // Signal for when card is insertted into the chip reader
+    void inserted();
+    // Signal for when a card is removed from the chip reader
+    void removed();
+    // Signal for when a card is tapped with NFC
+    void tapped();
+    // Signal for when a debit card is swiped
+    void debit();
+    // Signal for when a credit card is swiped
+    void credit();
+    // Signal for when a gift card is swiped
+    void giftCard();
+    // Signal for when payment method is mobile pay
+    void mobilePay();
+    // Signal for when cashback is requested for a specific amount
+    void cashbackRequested(double amount);
+    // Signal for when a keypad button was pressed
+    void pinpadKeyPressed(QString &key);
+    // Signal for when the screen of the card reader is pressed
+    void screenPressed();
+    // Signal for when the user's PIN is requested
+    void pinRequested();
+    // Signal for when the user cancels the PIN operation
+    void pinCanceled();
+    // Signal for when the user inputs a correct PIN
+    void pinAccepted();
+    // Signal for when the user inputs an incorrect PIN
+    void pinRejected();
+    // Signal for if the transaction is confirmed
+    void transactionConfirmed();
+    // Signal for if the transaction was declined
+    void transactionDeclined();
+
+    // Signal for any errors that occur while using the card reader
+    void error(QString &msg);
 };
 
 #endif // CARDREADER_H
--- src/statuslight.h
+++ src/statuslight.h
@@ -3,10 +3,38 @@
 
 #include "stationcomponent.h"
 
+#define OFF = 0;
+#define URGENT = 1;
+#define WAITING = 2;
+#define HELP_REQUESTED = 3;
+#define AGE_CHECK = 4;
+#define READY = 5;
+
+/* A class for communicating to the station status light. The status light has a few different status':
+ *
+ * OFF: the station is not active/is shutdown
+ *
+ * URGENT: the customer is in need of urgent assistance
+ *
+ * WAITING: the customer has been waiting for assistance for a while
+ *
+ * HELP_REQUESTED: the customer has requested for assistance
+ *
+ * READY: the station is ready for the next customer
+ * */
 class StatusLight : public StationComponent
 {
+    //Q_OBJECT
 public:
     explicit StatusLight(StationComponent *parent = nullptr);
+private:
+    short int status;
+
+signals:
+
+    // Signal for when status light has changed
+    void changed();
+
 };
 
 #endif // STATUSLIGHT_H
