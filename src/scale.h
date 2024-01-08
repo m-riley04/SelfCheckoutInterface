05e450b5beca195e85c210d9635649307c8d8eef
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-01-07 22:00:47 -0600

    add some methods, signals, and variables

--- src/scale.h
+++ src/scale.h
@@ -4,13 +4,22 @@
 #include "stationcomponent.h"
 #include <QObject>
 
+/* A class that represents the scale component for weighing foods and produce. Measures in pounds. */
 class Scale : public StationComponent
 {
     //Q_OBJECT
 public:
-    explicit Scale(StationComponent *parent = nullptr);
+    explicit Scale(double weight = 0.00, double maxWeight = 30.00, StationComponent *parent = nullptr);
+
+    void zero();
 
 signals:
+    void scaleMoved();
+    void itemWeighed();
+
+private:
+    double weight;
+    double maxWeight;
 };
 
 #endif // SCALE_H
